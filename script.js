13c61b6c7e417171de73369fa2035c4257efce01
stephenAmp <stephen013amponsem@gmail.com>
2024-01-31 22:01:53 +0800

    update

--- script.js
+++ script.js
@@ -1,27 +1,64 @@
 //  variables
-let attempts = 0;
+let attempts = 5;
 let randomNum = Math.floor(Math.random() * 100  + 1);
 
 const guess = document.getElementById("guess");
 const submit = document.getElementById("submit");
 const hint = document.getElementById("hint");
 const attemptText = document.getElementById("attempts");
+const replay = document.getElementById("replayButton");
+
 
 // Event Listeners
 submit.addEventListener("click",guessCheck);
-
-function guessCheck(){
+replay.addEventListener("click",resetGame)
+function guessCheck(event){
+    // event.preventDefault();
+    enableInput();
     const userValue = Number(guess.value);
-    attempts ++;
+    attempts --;
     if(userValue === randomNum){
         hint.textContent = "Congratulations! You won"
+        displayGameOver();
     } else if(userValue < randomNum){
-        hint.textContent = " Guessed number is low. Try again!"
+        hint.textContent = " Guessed number is low."
     }else{
-        hint.textContent=" guessed number is high. Try again!"
+        hint.textContent=" guessed number is high."
     }
-attemptText.textContent = "Attempt(s): "+ attempts;
-    if(attempts<=5){
-        
+attemptText.textContent = "Attempt(s) left: "+ attempts;
+    if(attempts <= 0){
+        displayGameOver(); 
     }
+ 
+}
+
+function reset(){
+    disableInput();
+    randomNum = Math.floor(Math.random()*100 +1);
+    attemptText.textContent=""
+    hint.textContent = ""
+    
+    
+}
+
+function disableInput(){
+    guess.disabled = true;
+    submit.disabled = true;
+}
+ function enableInput(){
+    guess.disabled = false;
+    submit.disabled = false;
+}
+
+function resetGame(){
+    reset();
+    enableInput()
+    replay.style.display = "none"
+    attempts =5;
+}
+
+function displayGameOver(){
+    replay.style.display ="flex";
+    disableInput()
+    attemptText.textContent = "GAME OVER"
 }
